abbe620830d23f6b69d8b2f6dd30fcda4583814e
Adrien Berchet <adrien.berchet@epfl.ch>
2021-07-09 11:23:41 +0200

    Change license and use version scm (#1)

--- luigi_tools/version.py
+++ luigi_tools/version.py
@@ -1,2 +1,5 @@
-"""Package version."""
-VERSION = "0.0.11.dev0"  # pragma: no cover
+"""The package version.
+
+This file was generated automatically, do not change it.
+"""
+__version__ = "0.0.11.dev1"
