3bcabc8faa634200bbf95cd4af9588d66e402450
Adrien Berchet <adrien.berchet@epfl.ch>
2023-02-07 14:29:53 +0100

    Fix coverage

--- luigi_tools/task.py
+++ luigi_tools/task.py
@@ -21,6 +21,7 @@ import luigi
 
 from luigi_tools.util import apply_over_luigi_iterable
 from luigi_tools.util import apply_over_outputs
+from luigi_tools.util import apply_over_required
 from luigi_tools.util import recursive_check
 from luigi_tools.util import target_remove
 
@@ -55,12 +56,9 @@ class RerunMixin:
 
         if isinstance(self, luigi.WrapperTask) and self.rerun:
 
-            def recursive_target_remove(target, *args, **kwargs):
-                outputs = getattr(target, "output", lambda: [])()
-                apply_over_luigi_iterable(outputs, target_remove)
-
-                reqs = getattr(target, "requires", lambda: [])()
-                apply_over_luigi_iterable(reqs, recursive_target_remove)
+            def recursive_target_remove(task, *args, **kwargs):
+                apply_over_luigi_iterable(task.output(), target_remove)
+                apply_over_required(task, recursive_target_remove)
 
             recursive_target_remove(self)
 
