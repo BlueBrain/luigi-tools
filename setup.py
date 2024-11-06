f1b14cc38e47045fa5ef11f1245aa8ff007e589e
Adrien Berchet <adrien.berchet@epfl.ch>
2024-11-06 09:16:46 +0100

    WIP

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     },
     license="Apache License 2.0",
     packages=find_namespace_packages(include=["luigi_tools*"]),
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     use_scm_version=True,
     setup_requires=[
         "setuptools_scm",
@@ -70,10 +70,10 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering",
     ],
 )
